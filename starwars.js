4a8f2543e4d5542e56cb37ec2f1be51350df4fa1
Mefew <matheus_i@hotmail.com>
2017-04-25 12:23:23 -0300

    "Que lindo" - disse Cobrão

--- starwars.js
+++ starwars.js
@@ -3,3 +3,18 @@
 // para carregar:
 //  - A lista de filmes
 //  - A introdução de cada filme, quando ele for clicado
+
+$.ajax({
+  url: 'http://swapi.co/api/films',
+  method: 'GET',
+  success: function(resposta) {
+    //let qtdeFilmes = resposta.count;
+    let filmes = resposta.results;
+    let htmlListaDeFilmes = document.getElementById('movies').getElementsByTagName('ul');
+    for (var filme of filmes) {
+      htmlListaDeFilmes.append("<li data-episode-url=\"http://swapi.co/api/films/"+filme.episode_id
+          +"/\">Episode "+filme.episode_id+ ": "+ filme.title "</li> ");
+      //<li data-episode-url="http://swapi.co/api/films/4/">Episode I</li>
+
+    }
+}});
